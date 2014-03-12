496f091cdabb5c08b893722b0ad015090132cf6d
Timothy Boronczyk <tboronczyk@gmail.com>
2014-03-12 15:52:53 -0400

    convert scanner test to proper unit test

--- tests/check_scanner.c
+++ tests/check_scanner.c
@@ -19,6 +19,7 @@
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#include <check.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,52 +28,95 @@
 #include "scanner.h"
 #include "parser.h"
 
-UFILE *ustdin,
-      *ustdout,
-      *ustderr;
+#define SCAN_AND_ASSERT(s,t) do { \
+    scanner_token((s)); \
+    u_fprintf(ustdout, "%d %S\n", s->name, s->tbuf); \
+    ck_assert_int_eq((s)->name, (t)); \
+} while (0)
 
-static void tokenize(Scanner *s) {
-    do {
-        Scanner_ErrCode err = scanner_token(s);
-        switch (err) {
-            case SCANERR_OK:
-                if (s->name == T_EOF) {
-                    (void)u_fprintf(ustdout, "Found %d <EOF>\n", s->name);
-                }
-                else {
-                    (void)u_fprintf(ustdout, "Found %d %S\n", s->name, s->tbuf);
-                }
-                break;
+Suite *scanner_suite(void);
 
-            case SCANERR_UNEXPECTED_LEX:
-                (void)u_fprintf(ustderr, "Scanner: Unexpected lexeme in %s %d:%d\n", s->fname, s->linenum, s->linepos);
-                return;
+START_TEST (test_scanner_tokenize)
+{
+    UFILE *ustdout = u_finit(stdout, NULL, NULL);
 
-            default:
-                (void)u_fprintf(ustderr, "Scanner: Error encountered (%d)\n", err);
-        }
-    }
-    while (s->name != T_EOF);
+    Scanner *s;
+    scanner_init(&s, "./extra/scanner-in01.dat");
+    SCAN_AND_ASSERT(s, T_ADD);
+    SCAN_AND_ASSERT(s, T_SUBTRACT);
+    SCAN_AND_ASSERT(s, T_MULTIPLY);
+    SCAN_AND_ASSERT(s, T_DIVIDE);
+    SCAN_AND_ASSERT(s, T_MODULO);
+    SCAN_AND_ASSERT(s, T_ASSIGN);
+    SCAN_AND_ASSERT(s, T_ADD_ASSIGN);
+    SCAN_AND_ASSERT(s, T_SUBTRACT_ASSIGN);
+    SCAN_AND_ASSERT(s, T_MULTIPLY_ASSIGN);
+    SCAN_AND_ASSERT(s, T_DIVIDE_ASSIGN);
+    SCAN_AND_ASSERT(s, T_MODULO_ASSIGN);
+    SCAN_AND_ASSERT(s, T_EQUAL);
+    SCAN_AND_ASSERT(s, T_NOT_EQUAL);
+    SCAN_AND_ASSERT(s, T_IS);
+    SCAN_AND_ASSERT(s, T_LESS);
+    SCAN_AND_ASSERT(s, T_LESS_EQUAL);
+    SCAN_AND_ASSERT(s, T_GREATER);
+    SCAN_AND_ASSERT(s, T_GREATER_EQUAL);
+    SCAN_AND_ASSERT(s, T_TRUE);
+    SCAN_AND_ASSERT(s, T_FALSE);
+    SCAN_AND_ASSERT(s, T_AND);
+    SCAN_AND_ASSERT(s, T_OR);
+    SCAN_AND_ASSERT(s, T_NOT);
+    SCAN_AND_ASSERT(s, T_IF);
+    SCAN_AND_ASSERT(s, T_ELSE);
+    SCAN_AND_ASSERT(s, T_FUNC);
+    SCAN_AND_ASSERT(s, T_RETURN);
+    SCAN_AND_ASSERT(s, T_WHILE);
+    SCAN_AND_ASSERT(s, T_VAR);
+    SCAN_AND_ASSERT(s, T_DOT);
+    SCAN_AND_ASSERT(s, T_CONCAT);
+    SCAN_AND_ASSERT(s, T_COMMA);
+    SCAN_AND_ASSERT(s, T_COLON);
+    SCAN_AND_ASSERT(s, T_LBRACE);
+    SCAN_AND_ASSERT(s, T_RBRACE);
+    SCAN_AND_ASSERT(s, T_LPAREN);
+    SCAN_AND_ASSERT(s, T_RPAREN);
+    SCAN_AND_ASSERT(s, T_COMMENT);
+    SCAN_AND_ASSERT(s, T_COMMENT);
+    SCAN_AND_ASSERT(s, T_COMMENT);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_STRING);
+    SCAN_AND_ASSERT(s, T_NUMBER);
+    SCAN_AND_ASSERT(s, T_NUMBER);
+    SCAN_AND_ASSERT(s, T_NUMBER);
+    SCAN_AND_ASSERT(s, T_NUMBER);
+    SCAN_AND_ASSERT(s, T_NUMBER);
+    SCAN_AND_ASSERT(s, T_IDENTIFIER);
+    SCAN_AND_ASSERT(s, T_IDENTIFIER);
+    SCAN_AND_ASSERT(s, T_EOF);
+    scanner_free(s);
+}
+END_TEST
+
+Suite *scanner_suite(void)
+{
+    Suite *s = suite_create("scanner");
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_scanner_tokenize);
+    suite_add_tcase(s, tc_core);
+
+    return s;
 }
 
 int main(void)
 {
-    Scanner *s;
-    Scanner_ErrCode err;
-
-    /* prepare unicode file descriptors */
-    ustdin  = u_finit(stdin,  NULL, NULL);
-    ustdout = u_finit(stdout, NULL, NULL);
-    ustderr = u_finit(stderr, NULL, NULL);
-
-    err = scanner_init(&s);
-    if (err != SCANERR_OK) {
-        fprintf(stderr, "Allocate scanner failed");
-        exit(EXIT_FAILURE);
-    }
-
-    tokenize(s);
-    scanner_free(s);
-
-    return EXIT_SUCCESS;
+    Suite *s = scanner_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    int fails = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (fails == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
