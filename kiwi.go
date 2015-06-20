860e9fac670d2c41bd5bf12fcb33fe1fa2cce2ee
Timothy Boronczyk <tboronczyk@gmail.com>
2015-06-19 22:13:59 -0400

    Driver can execute or display AST

--- kiwi.go
+++ kiwi.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"github.com/tboronczyk/kiwi/runtime"
+	"github.com/tboronczyk/kiwi/ast"
 	"github.com/tboronczyk/kiwi/parser"
+	"github.com/tboronczyk/kiwi/runtime"
 	"github.com/tboronczyk/kiwi/scanner"
 	"github.com/tboronczyk/kiwi/symtable"
 	"os"
 )
 
 func main() {
+	tree := flag.Bool("ast", false, "Display parsed abstract syntax tree")
+	flag.Parse()
+
 	r := bufio.NewReader(os.Stdin)
 	s := scanner.New(r)
 	p := parser.New()
 	p.InitScanner(s)
 
+	if *tree {
+		print(p)
+		return
+	}
+	exec(p)
+}
+
+func print(p parser.Parser) {
+	for {
+		n, err := p.Parse()
+		if n == nil {
+			if err != nil {
+				fmt.Println(err)
+			}
+			return
+		}
+		ast.Print(n)
+	}
+}
+
+func exec(p parser.Parser) {
 	varTable := symtable.New()
 	funTable := symtable.New()
 	runtime.LoadBuiltins(funTable)
